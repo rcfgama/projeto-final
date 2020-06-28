3233acf992d915968063ee6cfb05efe3a1dc316d
rcfgama <rcfgama@gmail.com>
2020-06-28 12:36:01 -0300

    Subindo os Arquvivos
    Aplicativo / Site projeto final do curso de Angular.

---
+++ src/app/calculadora/calculadora-routing,module.ts
@@ -0,0 +1,28 @@
+import { Routes, RouterModule } from '@angular/router';
+import { CalculadoraRoutingComponent } from './calculadora-routing.component';
+import { CalculadoraComponent } from './components';
+import { NgModule } from '@angular/core';
+
+export const CalculadoraRoutes: Routes = [
+    {
+        path: 'calculadora',
+        component: CalculadoraRoutingComponent,
+        children: [
+            {
+                path: '',
+                component: CalculadoraComponent
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(CalculadoraRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class CalculadoraRoutingModule {}
---
+++ src/app/dashboard/dados.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ];
+
+  constructor() { }
+
+  /**
+   * Retorna um observable contendo os dados a serem exibidos no gráfico.
+   * @returns Observable<any>
+   */
+  obterDados(): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete();
+    });
+  }
+}
